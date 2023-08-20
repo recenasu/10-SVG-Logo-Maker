71e73eec47fbf9a4cc0308f2a9ef5d31b27fb5de
Domitius <133902698+recenasu@users.noreply.github.com>
2023-08-20 16:49:42 -0700

    removed dev file shapes.js

--- shapes.js
+++
@@ -1,4 +0,0 @@
-    < svg viewBox = "0 0 300 200" xmlns = "http://www.w3.org/2000/svg" >
-        <rect x="30" y="20" width="80%" height="80%" fill="green"/>
-        <text x="84" y="120" font-family="Arial, Helvetica, sans-serif" font-style="normal" font-size="4em" fill="white">TFM</text>
-      </svg >
